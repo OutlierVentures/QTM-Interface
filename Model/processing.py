e75800872f439113c8454a0fd945b8dca79c4762
Anthony <anthony.katwan1@gmail.com>
2023-04-30 18:36:32 -0700

    Testing simulation and interface

--- Model/processing.py
+++ Model/processing.py
@@ -4,22 +4,29 @@ import pandas as pd
 
 
 
-def plot_stacked_area_graph(df, total_token_supply):
+def plot_stacked_area_graph(df):
     # pivot the dataframe to create a multi-level index with Investor_Name and timestep
     df_pivot = df.pivot(index='timestep', columns='Investor_Name', values='current_allocation')
     
-    # multiply the values by the total token supply to convert from percentage to token amount
-    df_pivot = df_pivot * total_token_supply
+    # create a list of x values (time steps)
+    x = df_pivot.index
     
-    # plot the stacked area graph
-    df_pivot.plot.area(stacked=True)
+    # create a list of y values (token allocations for each investor)
+    y = df_pivot.to_numpy().T
+    
+    # plot the stacked area graph using stackplot
+    plt.stackplot(x, y, labels=df_pivot.columns)
     
     # set the title and labels for the plot
     plt.title('Investor Allocation Over Time')
     plt.xlabel('Time Step')
     plt.ylabel('Token Allocation')
     
-    # show the plot
+    # format the y-axis labels as integers
+    plt.gca().yaxis.set_major_formatter('{:.0f}'.format)
+    
+    # show the legend and plot
+    plt.legend()
     plt.show()
 
 
---
+++ Model/simulation.py
@@ -0,0 +1,31 @@
+# Dependences
+import pandas as pd
+import numpy as np
+
+# radCAD
+from radcad import Model, Simulation, Experiment
+from radcad.engine import Engine, Backend
+
+import state_variables
+import state_update_blocks
+import sys_params
+from processing import *
+
+import importlib
+importlib.reload(state_variables)
+importlib.reload(state_update_blocks)
+importlib.reload(sys_params)
+
+import traceback
+
+
+MONTE_CARLO_RUNS = 1
+TIMESTEPS = 30
+
+model = Model(initial_state=state_variables.initial_state, params=sys_params.sys_param, state_update_blocks=state_update_blocks.state_update_block)
+simulation = Simulation(model=model, timesteps=TIMESTEPS, runs=MONTE_CARLO_RUNS)
+
+result = simulation.run()
+df = pd.DataFrame(result)
+
+investor_df = extract_investors(df)
