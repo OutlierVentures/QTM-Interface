7693c7e5589492ac67a2a142b79937062f5925b6
achimstruve <achim.struve@gmail.com>
2023-06-16 14:51:55 +0200

    restructured the parameter input section and included QTM derived parameter sets

--- Model/parts/utils.py
+++ Model/parts/utils.py
@@ -1,10 +1,89 @@
-def initial_investor_allocation(initial_investors,token_economy):
+import numpy as np
+import math
+import uuid
+import random
+from typing import *
+
+# Helper Functions
+def calculate_raised_capital(sys_param):
+    """
+    Calculate the overall raised capital from the initial investors
+    """
+    raised_capital = 0
+    for key, value in sys_param:
+        if "_raised" in key:
+            raised_capital += value
+
+    return raised_capital
+
+# Initialization
+def new_agent(stakeholder_type: str, usd_funds: int,
+              tokens: int, action_list: list, action_weights: Tuple) -> dict:
+    agent = {'type': stakeholder_type,
+             'usd_funds': usd_funds,
+             'tokens': tokens,
+             'tokens_vested': 0,
+             'tokens_locked': 0,
+             'action_list': action_list,
+             'action_weights': action_weights,
+             'current_action': 'hold'}
+    return agent
+
+
+def generate_agent(agents_dict):
+    initial_agents = {}
+    for a in agents_dict:
+        initial_agents[a] = new_agent(a['type'],
+                                    a['initial_usd_funds'],
+                                    a['initial_tokens'],
+                                    a['initial_tokens_vested'],
+                                    a['initial_tokens_locked'],
+                                    a['action_list'],
+                                    a['action_weights'],
+                                    a['current_action'])
+    return initial_agents
+
+# chatGPT solution
+def create_parameter_list(parameter_name, not_iterable_parameters, init_value, min, max, intervals):
+    if parameter_name in not_iterable_parameters:
+        return [init_value.replace(",","").replace("%","")]
+    else:
+        try:
+            if type(init_value) == str:
+                init_value = float(init_value.replace(",","").replace("%",""))
+            if type(min) == str:
+                min = float(min.replace(",","").replace("%",""))
+            if type(max) == str:
+                max = float(max.replace(",","").replace("%",""))
+            if type(intervals) == str and intervals != '':
+                intervals = int(float(intervals.replace(",","")).replace("%",""))
+        except ValueError:
+            return [init_value]
+        if math.isnan(min) or math.isnan(max) or math.isnan(intervals) or max<=min:
+            if max<=min:
+                print("Maximum parameter boundary is lower than minimum parameter boundary: Min: ", min, "; Max:", max, ". Using initial value ", init_value, " instead.")
+            return [float(init_value)]
+        else:
+            if math.isnan(intervals):
+                return [float(init_value)]
+            else:
+                return list(np.linspace(min, max, int(intervals)))
+
+def compose_initial_parameters(QTM_inputs, not_iterable_parameters):
+    initial_parameters = {}
+    for index, row in QTM_inputs.iterrows():
+        parameter_name = row['Parameter Name'].lower().replace(' ', '_').replace('/', '').replace('(', '').replace(')', '')
+        initial_parameters[parameter_name] = create_parameter_list(parameter_name, not_iterable_parameters, row['Initial Value'], row['Min'], row['Max'], row['Interval Steps'])
+    return initial_parameters
+
+
+def initial_investor_allocation(initial_stakeholder_values, token_economy):
     initial_token_supply = token_economy['initial_token_supply']
     external_equity = token_economy['external_equity']
 
-    initial_investors = fund_raising(initial_investors,initial_token_supply,external_equity)
+    initial_stakeholder_values = fund_raising(initial_stakeholder_values,initial_token_supply,external_equity)
 
-    for i, investor in initial_investors.items():
+    for i, investor in initial_stakeholder_values.items():
 
         if investor['percentage_allocation'] == 0 and investor['effective_token_price'] != 0:
             investor['percentage_allocation'] = (investor['capital_raise']/investor['effective_token_price'])/initial_token_supply
@@ -18,32 +97,32 @@ def initial_investor_allocation(initial_investors,token_economy):
         investor['current_allocation'] = initial_vesting * total_token_amount
 
 
-    initial_investors = create_liquidity_pool(initial_investors,initial_token_supply)
+    initial_stakeholder_values = create_liquidity_pool(initial_stakeholder_values,initial_token_supply)
 
-    token_economy['current_circulating_supply'] = sum([investor['current_allocation'] for i, investor in initial_investors.items()])
+    token_economy['current_circulating_supply'] = sum([investor['current_allocation'] for i, investor in initial_stakeholder_values.items()])
 
-    token_economy['sum_of_raised_capital'] = sum([investor['capital_raise'] for i, investor in initial_investors.items()])
+    token_economy['sum_of_raised_capital'] = sum([investor['capital_raise'] for i, investor in initial_stakeholder_values.items()])
 
-    return initial_investors, token_economy
-
-
+    return initial_stakeholder_values, token_economy
 
 
 
 
 
-def fund_raising(initial_investors,initial_token_supply,external_equity):
+
+
+def fund_raising(initial_stakeholder_values,initial_token_supply,external_equity):
 
 
     #need a public sale investor to be intiialized no matter what
-    initial_investors['Public Sale']['effective_token_price'] =  initial_investors['Public Sale']['valuation_cap']/initial_token_supply
-    initial_investors['Public Sale']['token_discount_perc'] = 0
-    public_token_price = initial_investors['Public Sale']['effective_token_price']
+    initial_stakeholder_values['Public Sale']['effective_token_price'] =  initial_stakeholder_values['Public Sale']['valuation_cap']/initial_token_supply
+    initial_stakeholder_values['Public Sale']['token_discount_perc'] = 0
+    public_token_price = initial_stakeholder_values['Public Sale']['effective_token_price']
 
 
 
     #Go through the rest of the investors that are not public sale
-    for i, investor in initial_investors.items():
+    for i, investor in initial_stakeholder_values.items():
         
 
         #Check if its an investor with a fundraise
@@ -57,7 +136,7 @@ def fund_raising(initial_investors,initial_token_supply,external_equity):
             #deal with angels
 
             if investor['subtype'] == "Angel":
-                investor['percentage_allocation'] = external_equity*(initial_investors['Founders']['percentage_allocation']/(1-external_equity))
+                investor['percentage_allocation'] = external_equity*(initial_stakeholder_values['Founders']['percentage_allocation']/(1-external_equity))
                 percentage_allocation = investor['percentage_allocation']
                                 
                 investor['valuation_cap'] = (investor['capital_raise'] / (percentage_allocation*initial_token_supply)) * initial_token_supply
@@ -80,28 +159,28 @@ def fund_raising(initial_investors,initial_token_supply,external_equity):
 
 
 
-    return initial_investors
+    return initial_stakeholder_values
 
 
 
-def create_liquidity_pool(initial_investors,initial_token_supply):
+def create_liquidity_pool(initial_stakeholder_values,initial_token_supply):
     percentage_allocation = 0
-    percentage_allocation = 1-sum([investor['percentage_allocation'] for i, investor in initial_investors.items()])
+    percentage_allocation = 1-sum([investor['percentage_allocation'] for i, investor in initial_stakeholder_values.items()])
     
 
 
-    initial_investors['Liquidity Pool']['percentage_allocation'] = percentage_allocation
+    initial_stakeholder_values['Liquidity Pool']['percentage_allocation'] = percentage_allocation
     total_token_amount = percentage_allocation * initial_token_supply
     
-    initial_vesting = initial_investors['Liquidity Pool']['initial_vesting']
-    initial_investors['Liquidity Pool']['current_allocation']= initial_vesting * total_token_amount
-    initial_investors['Liquidity Pool']['total_token_amount']= total_token_amount
+    initial_vesting = initial_stakeholder_values['Liquidity Pool']['initial_vesting']
+    initial_stakeholder_values['Liquidity Pool']['current_allocation']= initial_vesting * total_token_amount
+    initial_stakeholder_values['Liquidity Pool']['total_token_amount']= total_token_amount
 
-    return initial_investors
+    return initial_stakeholder_values
 
 
 
-def liquidity_pool_module(initial_investors,token_economy,liquidity_pool):
+def liquidity_pool_module(initial_stakeholder_values,token_economy,liquidity_pool):
 
 
     public_sale_valuation = token_economy['public_sale_valuation']
@@ -116,7 +195,7 @@ def liquidity_pool_module(initial_investors,token_economy,liquidity_pool):
 
     token_launch_price = public_sale_valuation / initial_token_supply
 
-    liquidity_pool_fund_allocation = token_launch_price*initial_investors['Liquidity Pool']['total_token_amount']*100
+    liquidity_pool_fund_allocation = token_launch_price*initial_stakeholder_values['Liquidity Pool']['total_token_amount']*100
 
     allocation_by_raised_amount = liquidity_pool_fund_allocation/sum_of_raised_capital
 
--- Model/sys_params.py
+++ Model/sys_params.py
@@ -1,175 +1,146 @@
-
-initial_investors = {
-         'Public Sale': {
-         'percentage_allocation': .035,
-         'initial_vesting': .25,
-         'cliff_in_months': 0,
-         'total_months': 6,
-         'current_allocation': 0,
-         'type':"Investor",
-         'subtype': "Public Sale",
-         'total_token_amount':0,
-         'capital_raise': 2100000,
-         'valuation_cap': 60000000,
-         'bonus_amount':0,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-        
-        'Liquidity Pool': {
-         'percentage_allocation': 0,
-         'initial_vesting': 1,
-         'cliff_in_months': 0,
-         'total_months': 0,
-         'current_allocation': 0,
-         'type':"Investor",
-         'subtype': "Public Sale",
-         'total_token_amount':0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'bonus_amount':0,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-
-         'Angels': {
-         'percentage_allocation': 0,
-         'initial_vesting': 0,
-         'cliff_in_months': 1,
-         'total_months': 24,
-         'current_allocation': 0,
-         'type':"Investor",
-         'subtype': "Angel",
-         'total_token_amount':0,
-         'capital_raise': 250000,
-         'valuation_cap': 0,
-         'bonus_amount':0,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-        
-        'Seed': { 
-         'percentage_allocation': 0,
-         'initial_vesting': 0,
-         'cliff_in_months': 1,
-         'total_months': 24,
-         'current_allocation': 0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'type':"Investor",
-         'subtype': "Fundrasing",
-         'total_token_amount':0,
-         'capital_raise': 750000,
-         'valuation_cap': 30000000,
-         'bonus_amount':0.5,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-        
-         'Pre-Sale 1': {
-         'percentage_allocation': 0,
-         'initial_vesting': 0,
-         'cliff_in_months': 1,
-         'total_months': 12,
-         'current_allocation': 0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'type':"Investor",
-         'subtype': "Fundrasing",
-         'total_token_amount':0,
-         'capital_raise': 1500000,
-         'valuation_cap': 40000000,
-         'bonus_amount':0.25,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-
-         'Pre-Sale 2': {
-         'percentage_allocation': 0,
-         'initial_vesting': 0,
-         'cliff_in_months': 1,
-         'total_months': 9,
-         'current_allocation': 0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'type':"Investor",
-         'subtype': "Fundrasing",
-         'total_token_amount':0,
-         'capital_raise': 2500000,
-         'valuation_cap': 50000000,
-         'bonus_amount':0.15,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-
-         'Founders': {
-         'percentage_allocation': .25,
-         'initial_vesting': 0,
-         'cliff_in_months': 6,
-         'total_months': 36,
-         'current_allocation': 0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'type':"Investor",
-         'subtype': "None",
-         'total_token_amount':0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'bonus_amount':0,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-
-         'Advisors': {
-         'percentage_allocation': 0.01,
-         'initial_vesting': 0,
-         'cliff_in_months': 1,
-         'total_months': 24,
-         'current_allocation': 0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'type':"Investor",
-         'subtype': "None",
-         'total_token_amount':0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'bonus_amount':0,
-         'effective_token_price':0,
-         'token_discount_perc':0 },
-
-         'Reserve': {
-         'percentage_allocation': 0.4,
-         'initial_vesting': 1,
-         'cliff_in_months': 0,
-         'total_months': 0,
-         'current_allocation': 0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'type':"Project Buckets",
-         'subtype': "None",
-         'total_token_amount':0,
-         'capital_raise': 0,
-         'valuation_cap': 0,
-         'bonus_amount':0,
-         'effective_token_price':0,
-         'token_discount_perc':0 }
-
-    }
-    
-token_economy = {
-    'initial_token_supply': 100000000, # Total number of tokens initially in the economy
-    'public_sale_valuation': 60000000,
-    'current_circulating_supply':0,
-    'external_equity': .1,
-    'inflation': 'Fixed',
-    'sum_of_raised_capital':0
-}
-
-liquidity_pool ={
-    'token_lp_weighting': .5,
-    'lp_paring_token_ticker': 'USDC',
-    'lp_paring_token_price': 1
-}
+import uuid
+from parts.utils import *
+import pandas as pd
+import sys
+sys.path.append('../')
+from data.not_iterable_variables import *
 
 
+QTM_inputs = pd.read_csv('../data/Quantitative_Token_Model_V1.87 - cadCAD_inputs.csv')
 
 # System parameters
-sys_param = {
-    'token_economy': token_economy,
-    'initial_investors':initial_investors,
-    'liquidity_pool': liquidity_pool  
-    
+sys_param = compose_initial_parameters(QTM_inputs, parameter_list)
+
+# initial values
+foundation_agent = new_agent('foundation', initial_foundation_usd_funds, initial_foundation_tokens, ['trade', 'hold', 'lock', 'remove_locked_tokens', 'incentivise'], (0,50,0,0,50))
+early_investor_agent = new_agent('early_investor', initial_early_investor_usd_funds, initial_early_investor_tokens, ['trade', 'hold', 'lock', 'remove_locked_tokens'], (60,20,6,14))
+market_investor_agent = new_agent('market_investor', initial_market_investor_usd_funds, initial_market_investor_tokens, ['trade', 'hold', 'lock', 'remove_locked_tokens'], (60,15,16,9))
+
+agent_behavior_dict = {
+    'angle': {
+        'trade': sys_param['avg_token_selling_allocation']-sys_param['avg_token_utility_removal']/3,
+        'hold': sys_param['avg_token_holding_allocation']-sys_param['avg_token_utility_removal']/3,
+        'utility': sys_param['avg_token_utility_allocation']-sys_param['avg_token_utility_removal']/3,
+        'remove_locked_tokens': sys_param['avg_token_utility_removal'],
+    },
+    'seed': {
+        'trade': sys_param['avg_token_selling_allocation']-sys_param['avg_token_utility_removal']/3,
+        'hold': sys_param['avg_token_holding_allocation']-sys_param['avg_token_utility_removal']/3,
+        'utility': sys_param['avg_token_utility_allocation']-sys_param['avg_token_utility_removal']/3,
+        'remove_locked_tokens': sys_param['avg_token_utility_removal'],
+    },
+    'presale_1': {
+        'trade': sys_param['avg_token_selling_allocation']-sys_param['avg_token_utility_removal']/3,
+        'hold': sys_param['avg_token_holding_allocation']-sys_param['avg_token_utility_removal']/3,
+        'utility': sys_param['avg_token_utility_allocation']-sys_param['avg_token_utility_removal']/3,
+        'remove_locked_tokens': sys_param['avg_token_utility_removal'],
+    },
+    'presale_2': {
+        'trade': sys_param['avg_token_selling_allocation']-sys_param['avg_token_utility_removal']/3,
+        'hold': sys_param['avg_token_holding_allocation']-sys_param['avg_token_utility_removal']/3,
+        'utility': sys_param['avg_token_utility_allocation']-sys_param['avg_token_utility_removal']/3,
+        'remove_locked_tokens': sys_param['avg_token_utility_removal'],
+    },
+    'public_sale': {
+        'trade': sys_param['avg_token_selling_allocation']-sys_param['avg_token_utility_removal']/3,
+        'hold': sys_param['avg_token_holding_allocation']-sys_param['avg_token_utility_removal']/3,
+        'utility': sys_param['avg_token_utility_allocation']-sys_param['avg_token_utility_removal']/3,
+        'remove_locked_tokens': sys_param['avg_token_utility_removal'],
+    },
+    'team': {
+        'trade': sys_param['avg_token_selling_allocation']-sys_param['avg_token_utility_removal']/3,
+        'hold': sys_param['avg_token_holding_allocation']-sys_param['avg_token_utility_removal']/3,
+        'utility': sys_param['avg_token_utility_allocation']-sys_param['avg_token_utility_removal']/3,
+        'remove_locked_tokens': sys_param['avg_token_utility_removal'],
+    },
+    'reserve': {
+        'trade': 0,
+        'hold': 50,
+        'utility': 0,
+        'remove_locked_tokens': 0,
+        'incentivise': 50
+    },
+    'community': {
+        'trade': 0,
+        'hold': 100,
+        'utility': 0,
+        'remove_locked_tokens': 0,
+        'incentivise': 0
+    },
+    'foundation': {
+        'trade': 0,
+        'hold': 100,
+        'utility': 0,
+        'remove_locked_tokens': 0,
+        'incentivise': 0
+    },
+    'placeholder_1': {
+        'trade': 0,
+        'hold': 100,
+        'utility': 0,
+        'remove_locked_tokens': 0,
+        'incentivise': 0
+    },
+    'placeholder_1': {
+        'trade': 0,
+        'hold': 100,
+        'utility': 0,
+        'remove_locked_tokens': 0,
+        'incentivise': 0
+    },
+}
+
+initial_values = {
+    "initial_agent_values" : {
+        uuid.uuid4(): {
+            'type': 'team',
+            'initial_usd_funds': 0,
+            'initial_tokens': 0,
+            'initial_tokens_vested': 0,
+            'initial_tokens_locked': 0,
+            'action_list': list(agent_behavior_dict['team'].keys()),
+            'action_weights': tuple(agent_behavior_dict['team'].values()),
+            'current_action': 'hold'
+        },
+        uuid.uuid4(): {
+            'type': 'reserve',
+            'initial_usd_funds': calculate_raised_capital(sys_param),
+            'initial_tokens': (sys_param['reserve_initial_vesting']/100) * (sys_param['reserve_allocation']/100) * sys_param['initial_total_supply'],
+            'initial_tokens_vested': (sys_param['reserve_initial_vesting']/100) * (sys_param['reserve_allocation']/100) * sys_param['initial_total_supply'],
+            'initial_tokens_locked': 0,
+            'action_list': list(agent_behavior_dict['reserve'].keys()),
+            'action_weights': tuple(agent_behavior_dict['reserve'].values()),
+            'current_action': 'hold'
+        },
+        uuid.uuid4(): {
+            'type': 'foundation',
+            'initial_usd_funds': 0,
+            'initial_tokens': 0,
+            'initial_tokens_vested': 0,
+            'initial_tokens_locked': 0,
+            'action_list': ['trade', 'hold', 'lock', 'remove_locked_tokens', 'incentivise'],
+            'action_weights': (0,100,0,0,0),
+            'current_action': 'hold'
+        },
+        uuid.uuid4(): {
+            'type': 'team',
+            'initial_usd_funds': 0,
+            'initial_tokens': 0,
+            'initial_tokens_vested': 0,
+            'initial_tokens_locked': 0,
+            'action_list': ['trade', 'hold', 'lock', 'remove_locked_tokens'],
+            'action_weights': (0,0,100,0),
+            'current_action': 'hold'
+        },
+        uuid.uuid4(): {
+            'type': 'team',
+            'initial_usd_funds': 0,
+            'initial_tokens': 0,
+            'initial_tokens_vested': 0,
+            'initial_tokens_locked': 0,
+            'action_list': ['trade', 'hold', 'lock', 'remove_locked_tokens'],
+            'action_weights': (0,0,100,0),
+            'current_action': 'hold'
+        },
+    },
 }
