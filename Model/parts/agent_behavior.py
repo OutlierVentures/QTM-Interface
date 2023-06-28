a1ebb54e8df05f07dad42a801b37b587ff5030eb
achimstruve <achim.struve@gmail.com>
2023-06-28 10:01:51 +0200

    added agent_token_allocation policy to account for the static agent behavior

--- Model/parts/agent_behavior.py
+++ Model/parts/agent_behavior.py
@@ -92,6 +92,7 @@ def generate_agent_behavior(params, substep, state_history, prev_state, **kwargs
     elif params['agent_behavior'] == 'static':
         """
         Define the agent behavior for each agent type for the static 1:1 QTM behavior
+        ToDo: Consistency checks of correct meta bucket and utility share amounts, which should be 100% in total for each agent type
         """
         agents = prev_state['agents'].copy()
         
@@ -105,16 +106,19 @@ def generate_agent_behavior(params, substep, state_history, prev_state, **kwargs
                 'hold': params['avg_token_holding_allocation'],
                 'utility': params['avg_token_utility_allocation'],
                 'remove_locked_tokens': params['avg_token_utility_removal'],
-                'incentivise': 0
+                'locking': params['lock_share'],
+                'liquidity': params['liquidity_mining_share'],
+                'transfer': params['transfer_share'],
+                'burning': params['burning_share']
             }
 
     return {'agent_behavior_dict': agent_behavior_dict}
 
-def meta_bucket_token_allocations(params, substep, state_history, prev_state, **kwargs):
+def agent_token_allocations(params, substep, state_history, prev_state, **kwargs):
     """
     Define the meta bucket token allocations of all agents with respect to 'sell' 'hold' and 'utility'
     """
-    updated_agents = prev_state['agents'].copy()
+    agents = prev_state['agents']
 
     # initialize meta bucket token allocations
     meta_bucket_allocations= {
@@ -124,31 +128,71 @@ def meta_bucket_token_allocations(params, substep, state_history, prev_state, **
         'removed': 0
     }
 
+    utility_bucket_allocations = {
+        'locking': 0,
+        'liquidity': 0,
+        'transfer': 0,
+        'burn': 0
+    }
+
     # update agent token allocations and update the meta bucket allocations w.r.t. each agents contribution
     # note that protocol buckets are not used for meta bucket allocations
-    for agent in updated_agents:
-        if updated_agents[agent]['type'] != 'protocol_bucket':
+    agent_allocations = {}
+    for agent in agents:
+        if agents[agent]['type'] != 'protocol_bucket':
             
             # get agent static behavior indices for behavior list
-            behavior_lst_sell_index = updated_agents[agent]['action_list'].index('trade')
-            behavior_lst_utility_index = updated_agents[agent]['action_list'].index('utility')
-            behavior_lst_remove_index = updated_agents[agent]['action_list'].index('remove_locked_tokens')
+            behavior_lst_sell_index = agents[agent]['action_list'].index('trade')
+            behavior_lst_utility_index = agents[agent]['action_list'].index('utility')
+            behavior_lst_remove_index = agents[agent]['action_list'].index('remove_locked_tokens')
+            behavior_lst_locking_index = agents[agent]['action_list'].index('locking')
+            behavior_lst_liquidity_index = agents[agent]['action_list'].index('liquidity')
+            behavior_lst_transfer_index = agents[agent]['action_list'].index('transfer')
+            behavior_lst_burning_index = agents[agent]['action_list'].index('burning')
 
             # get agent static behavior percentages
-            selling_perc = updated_agents[agent]['action_weights'][behavior_lst_sell_index]
-            utility_perc = updated_agents[agent]['action_weights'][behavior_lst_utility_index]
-            remove_perc = updated_agents[agent]['action_weights'][behavior_lst_remove_index]
+            selling_perc = agents[agent]['action_weights'][behavior_lst_sell_index]
+            utility_perc = agents[agent]['action_weights'][behavior_lst_utility_index]
+            remove_perc = agents[agent]['action_weights'][behavior_lst_remove_index]
+            locking_perc = agents[agent]['action_weights'][behavior_lst_locking_index]
+            liquidity_perc = agents[agent]['action_weights'][behavior_lst_liquidity_index]
+            transfer_perc = agents[agent]['action_weights'][behavior_lst_transfer_index]
+            burn_perc = agents[agent]['action_weights'][behavior_lst_burning_index]
 
             # calculate corresponding absolute token amounts for meta buckets
-            sold_tokens = updated_agents[agent]['tokens'] * selling_perc/100
-            utility_tokens = updated_agents[agent]['tokens'] * utility_perc/100
-            removed_tokens = updated_agents[agent]['tokens'] * utility_perc/100
-            
-            updated_agents[agent]['tokens'] -= (sold_tokens + utility_tokens)
+            sold_tokens = agents[agent]['tokens'] * selling_perc/100
+            utility_tokens = agents[agent]['tokens'] * utility_perc/100
+            removed_tokens = (agents[agent]['tokens_locked'] + agents[agent]['tokens_liquidity_provisioning']) * remove_perc/100
+            locked_tokens = utility_tokens * locking_perc/100
+            liquidity_tokens = utility_tokens * liquidity_perc/100
+            transfer_tokens = utility_tokens * transfer_perc/100
+            burn_tokens = utility_tokens * burn_perc/100
 
-        
+            # populate meta bucket allocations
+            meta_bucket_allocations['selling'] += sold_tokens
+            meta_bucket_allocations['holding'] += agents[agent]['tokens'] - sold_tokens - utility_tokens + removed_tokens
+            meta_bucket_allocations['utility'] += utility_tokens
+            meta_bucket_allocations['removed'] += removed_tokens
 
-    return {'meta_bucket_token_allocations': meta_bucket_token_allocations}
+            # populate utility bucket allocations
+            utility_bucket_allocations['locking'] += locked_tokens
+            utility_bucket_allocations['liquidity'] += liquidity_tokens
+            utility_bucket_allocations['transfer'] += transfer_tokens
+            utility_bucket_allocations['burn'] += burn_tokens
+
+            # update agent token allocations
+            agent_allocations[agent] = {
+                'selling': sold_tokens,
+                'holding': agents[agent]['tokens'] - sold_tokens - utility_tokens + removed_tokens,
+                'utility': utility_tokens,
+                'removed': removed_tokens,
+                'locking': locked_tokens,
+                'liquidity': liquidity_tokens,
+                'transfer': transfer_tokens,
+                'burn': burn_tokens
+            }
+
+    return {'meta_bucket_allocations': meta_bucket_allocations, 'utility_bucket_allocations': utility_bucket_allocations, 'agent_allocations': agent_allocations}
 
 
 # STATE UPDATE FUNCTIONS
