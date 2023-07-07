6e47bcd8dac5698dca22c20cf1c8772bca8f4615
Anthony <anthony.katwan1@gmail.com>
2023-07-07 15:27:55 -0700

    Adoption Metric Post processing clean up

--- Model/simulation.py
+++ Model/simulation.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
 
     ##---User adoption Plots---#
-    #plot_line_chart(data,'timestep',['token_holders','product_users'])
+    plot_line_chart(data,'timestep',['token_holders','product_users'])
     #plot_line_chart(data,'timestep',['product_revenue'])
     #plot_line_chart(data,'timestep',['token_buys'])
 
