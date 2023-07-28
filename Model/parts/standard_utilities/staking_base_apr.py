2913aad1ea30d894d408af2614f24288de99e098
Anthony <anthony.katwan1@gmail.com>
2023-07-27 18:59:43 -0700

    adjusted utilities files, fixed path on test stage

--- Model/parts/standard_utilities/staking_base_apr.py
+++ Model/parts/standard_utilities/staking_base_apr.py
@@ -31,19 +31,3 @@ def update_staking_base_apr(params, substep, state_history, prev_state, policy_i
 
 
 
-
-##---staking_buyback_distribute---###
-
-# POLICY FUNCTIONS
-def staking_buyback_distribute(params, substep, state_history, prev_state, **kwargs):
-
-
-
-   
-    staking_base_apr_cum = 0    #prev_staking_base_apr_cum + staking_base_apr+removal_token_staking_base_apr
-    return ('staking_base_apr_cum',staking_base_apr_cum)
-
-
-# STATE UPDATE FUNCTIONS
-def update_staking_buyback_distribute(params, substep, state_history, prev_state, policy_input, **kwargs):
-    return
