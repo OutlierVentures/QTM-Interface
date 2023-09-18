b6fee1756eaf83cad1ab537ebf36a835b8e8cdca
Anthony <anthony.katwan1@gmail.com>
2023-09-17 21:14:23 -0700

    interface with test data and simulation run

--- Model/interface.py
+++ Model/interface.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import os
+import subprocess
+
 
 st.title('QTM File Upload')
 
@@ -25,3 +27,34 @@ if uploaded_file is not None:
     
     st.write(f"File '{uploaded_file.name}' uploaded successfully!")
 
+
+
+
+
+
+
+if st.button('Run Simulation'):
+    # Set a session state variable to indicate the button has been clicked
+    st.session_state.run_simulation_clicked = True
+
+# Check the session state variable to determine whether to run the simulation
+if st.session_state.run_simulation_clicked:
+    # Code to run the simulation goes here
+    st.markdown("<div style='background-color:blue; padding:10px; border-radius:5px;'>Simulation running...</div>", unsafe_allow_html=True)
+    
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_path = os.path.join(script_dir, 'simulation.py')
+    
+    # Run the simulation.py script
+    result = subprocess.run(['python', script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    if result.returncode == 0:
+        st.markdown("<div style='background-color:green; padding:10px; border-radius:5px;'>Simulation completed successfully!</div>", unsafe_allow_html=True)
+        st.write(result.stdout.decode('utf-8'))
+    else:
+        st.markdown("<div style='background-color:red; padding:10px; border-radius:5px;'>Simulation encountered an error.</div>", unsafe_allow_html=True)
+        st.write(result.stderr.decode('utf-8'))
+    
+    # Reset the session state variable after running the simulation
+    st.session_state.run_simulation_clicked = False
--- Model/simulation.py
+++ Model/simulation.py
@@ -1,14 +1,32 @@
-# Dependencies
+# Dependences
 import pandas as pd
 import numpy as np
+import os
+import sys
+import traceback
+import time
+import streamlit as st
 
 # radCAD
 from radcad import Model, Simulation, Experiment
 from radcad.engine import Engine, Backend
 
+
+# Project dependences
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Go up one folder
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+# Append the parent directory to sys.path
+sys.path.append(parent_dir)
+
+from sys_params import *
 import state_variables
 import state_update_blocks
 import sys_params
+from parts.utils import *
 from plots import *
 from post_processing import *
 
@@ -17,9 +35,24 @@ importlib.reload(state_variables)
 importlib.reload(state_update_blocks)
 importlib.reload(sys_params)
 
-import traceback
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Go two folders up
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__'   :
+    start_time = time.process_time()
+
     MONTE_CARLO_RUNS = 1
     TIMESTEPS = 12*10
 
@@ -28,7 +61,20 @@ if __name__ == '__main__'   :
 
     result = simulation.run()
     df = pd.DataFrame(result)
+    simulation_end_time = time.process_time()
 
     # post processing
-    data = postprocessing(df, substep=df.substep.max())
+    data_tx1 = postprocessing(df, substep=15) # after adoption buy lp tx
+    data_tx2 = postprocessing(df, substep=18) # after vesting sell lp tx
+    data_tx3 = postprocessing(df, substep=19) # after vesting sell lp tx
+    data_tx4 = postprocessing(df, substep=20) # after vesting sell lp tx
+    postprocessing_one_start_time = time.process_time()
+    data = postprocessing(df, substep=df.substep.max()) # at the end of the timestep = last substep
+    postprocessing_all_end_time = time.process_time()
 
+
+    # display necessery time data
+    print("Simulation time: ", simulation_end_time - start_time, " s")
+    print("Post processing one dataframe time: ", postprocessing_all_end_time - postprocessing_one_start_time, " s")
+    print("Post processing all dataframes time: ", postprocessing_all_end_time - simulation_end_time, " s")
+    print("Whole Test time: ", time.process_time() - start_time, " s")
