82eb802dec06eec01ee56e115ff2bee21f7ac108
Anthony <anthony.katwan1@gmail.com>
2023-09-20 17:33:22 -0700

    Print statements to help track time

--- Model/interface.py
+++ Model/interface.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import pandas as pd
 import sqlite3
+import time
+
 
 st.title('QTM File Upload')
 
@@ -29,48 +31,67 @@ if uploaded_file is not None:
     st.write(f"File '{uploaded_file.name}' uploaded successfully!")
 
 
-if 'run_simulation_clicked' not in st.session_state:
-    st.session_state['run_simulation_clicked'] = False
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+script_path = os.path.join(script_dir, 'simulation.py')
+    
+
+if 'button_clicked' not in st.session_state:
+    st.session_state.button_clicked = False
 
 if st.button('Run Simulation'):
-    # Set a session state variable to indicate the button has been clicked
-    st.session_state.run_simulation_clicked = True
+    st.session_state.button_clicked = True
 
-# Check the session state variable to determine whether to run the simulation
-if st.session_state.run_simulation_clicked:
-    # Code to run the simulation goes here
-    st.markdown("<div style='background-color:blue; padding:10px; border-radius:5px;'>Simulation running...</div>", unsafe_allow_html=True)
+if st.session_state.button_clicked:
+    buttonS = time.process_time()
+
+    markS = time.process_time()
     
-    # Get the directory of the current script
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    script_path = os.path.join(script_dir, 'simulation.py')
+    st.markdown("<div style='background-color:blue; padding:10px; border-radius:5px;'>Simulation running...</div>", unsafe_allow_html=True)
     
     # Run the simulation.py script
     result = subprocess.run(['python', script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
+    markE = time.process_time()
+    print("Script Run Time:", markE - markS)
+
     if result.returncode == 0:
         st.markdown("<div style='background-color:green; padding:10px; border-radius:5px;'>Simulation completed successfully!</div>", unsafe_allow_html=True)
         st.write(result.stdout.decode('utf-8'))
     else:
         st.markdown("<div style='background-color:red; padding:10px; border-radius:5px;'>Simulation encountered an error.</div>", unsafe_allow_html=True)
         st.write(result.stderr.decode('utf-8'))
-    
-    # Reset the session state variable after running the simulation
-    st.session_state.run_simulation_clicked = False
 
+    dataStart = time.process_time()
 
     # Connect to the SQLite database
     conn = sqlite3.connect('interfaceData.db')
-    
     # Read the data from the SQLite table into a DataFrame
     df = pd.read_sql('SELECT * FROM simulation_data', conn)
-    
+
+
+    dataEnd = time.process_time()
+
+    print("Data Run Time", dataEnd - dataStart)
+
     # Close the connection
     conn.close()
-    
-    # Now `df` holds your data as a Pandas DataFrame, and you can work with it in your Streamlit app
-    st.write(df)
-    print(df)
 
+    # Now `df` holds your data as a Pandas DataFrame, and you can work with it in your Streamlit app
+    dataWriteS = time.process_time()
+    st.write(df)
+    dataWriteE = time.process_time()
+    print("Write Run Time", dataWriteE - dataWriteS)
+
+    buttonE = time.process_time()
+    print("Full Button Run Time", buttonE - buttonS)
+
+    # Reset the session state variable after running the simulation
+    st.session_state.button_clicked = False
+
+
+
+ 
+
+ 
 
