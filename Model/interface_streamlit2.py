67904b17248a7357f28693b1e4a0e007553a8e3d
achimstruve <achim.struve@gmail.com>
2023-10-04 14:01:01 +0200

    parts: add working file upload capability to interface_streamlit2.py

--- Model/interface_streamlit2.py
+++ Model/interface_streamlit2.py
@@ -14,8 +14,29 @@ sys.path.append(parent_dir)
 input_file_base_path = parent_dir+'/data/'
 
 st.title('Quantitative Token Model')
-input_file_name = st.text_input('Input File Name', 'Quantitative_Token_Model_V1.89_radCad_integration - radCAD_inputs.csv')
-input_file_path = input_file_base_path + input_file_name
+
+
+# Upload Input File
+st.markdown("Use the default input file or upload your own.")
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    # Get the file name and construct the full path
+    input_file_name = uploaded_file.name
+    input_file_path = os.path.join(input_file_base_path, input_file_name)
+    
+    # Check if the file already exists
+    if os.path.exists(input_file_path):
+        st.write(f"File '{uploaded_file.name}' already exists, overwriting...")
+    
+    # Write the uploaded file's content to the destination file
+    with open(input_file_path, 'wb') as f:
+        f.write(uploaded_file.getvalue())
+    
+    st.write(f"File '{uploaded_file.name}' uploaded successfully!")
+else:
+    input_file_name = 'Quantitative_Token_Model_V1.89_radCad_integration - radCAD_inputs.csv'
+    input_file_path = input_file_base_path + input_file_name
 
 
 
@@ -25,6 +46,7 @@ if 'button_clicked' not in st.session_state:
 if st.button('Run Simulation'):
     st.session_state['button_clicked'] = True
 if 'button_clicked' in st.session_state and st.session_state['button_clicked']:
+    st.markdown("Use "+input_file_name+" as input file for the simulation...")
     # Run the simulation.py script
     result = simulation(input_file_path)
     # Reset the session state variable after running the simulation
