dedfab1686b667346db761d8a199d8fafe799e9b
achimstruve <achim.struve@gmail.com>
2023-10-26 18:06:56 +0200

    feat: add Business Assumptions streamlit input section

--- UserInterface/helpers.py
+++ UserInterface/helpers.py
@@ -492,7 +492,7 @@ def model_ui_inputs(input_file_path, uploaded_file, parameter_list):
 
 
     st.markdown("### User Adoption")
-    # adoption style choice | user numbers | revenues
+    # adoption style choice | user numbers | revenues | meta bucket allocations
     col61, col62, col63 = st.columns(3)
     with col61:
         adoption_style = st.radio('Adoption Assumption',('Weak', 'Medium', 'Strong', 'Custom'), index=0, help='The adoption style determines the scaling velocity for the product revenue and token demand. Moreover it influences the average agent sentiment in terms of selling and utility adoption behavior.')
@@ -584,6 +584,80 @@ def model_ui_inputs(input_file_path, uploaded_file, parameter_list):
         avg_token_holder_growth_rate = adoption_dict[adoption_style]['avg_token_holder_growth_rate']
         token_holders_after_10y = initial_token_holders * (1 + avg_token_holder_growth_rate/100)**120
 
+
+    st.markdown("### Business Assumptions")
+    # income | expenditures | buybacks | burns
+    st.write("**Financial Streams**")
+    col81, col82, col83 = st.columns(3)
+    with col81:
+        show_full_business_table = st.toggle('Use Full Custom Table', value=False, help="Show the full business assumption parameter set.")
+        if not show_full_business_table:
+            income = st.number_input('Additional income per month / $k', label_visibility="visible", min_value=0.0, value=float(sys_param['royalty_income_per_month'][0] + sys_param['other_income_per_month'][0] + sys_param['treasury_income_per_month'][0])/1e3, disabled=False, key="income", help="The monthly income for the business on top of the product revenue, defined in the user adoption section.")
+            expenditures = st.number_input('Expenditures per month / $k', label_visibility="visible", min_value=0.0, value=float(sys_param['salaries_per_month'][0] + sys_param['license_costs_per_month'][0] + sys_param['other_monthly_costs'][0] + (sys_param['one_time_payments_1'][0]+ sys_param['one_time_payments_2'][0])/120)/1e3, disabled=False, key="expenditures", help="The monthly expenditures for the business.")
+    if show_full_business_table:
+        with col82:
+            st.write("**Income**")
+            royalty_income_per_month = st.number_input('Royalty income per month / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['royalty_income_per_month'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="royalty_income_per_month", help="The monthly royalty income for the business.")
+            treasury_income_per_month = st.number_input('Treasury income per month / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['treasury_income_per_month'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="treasury_income_per_month", help="The monthly income for the business from treasury investments yields.")
+            other_income_per_month = st.number_input('Other income per month / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['other_income_per_month'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="other_income_per_month", help="The monthly income for the business from other sources.")
+        with col83:
+            st.write("**Expenditures**")
+            one_time_payments_1 = st.number_input('One-time payments / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['one_time_payments_1'][0] + sys_param['one_time_payments_2'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="one_time_payments_1", help="The one-time payments for the business at launch (e.g. any back payment of liabilities or treasury investments).")
+            salaries_per_month = st.number_input('Salaries per month / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['salaries_per_month'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="salaries_per_month", help="The monthly salaries paid by the business.")
+            license_costs_per_month = st.number_input('License costs per month / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['license_costs_per_month'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="license_costs_per_month", help="The monthly license costs paid by the business.")
+            other_monthly_costs = st.number_input('Other monthly costs / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['other_monthly_costs'][0])/1e3 if show_full_business_table else 0.0][0], disabled=False, key="other_monthly_costs", help="The monthly costs paid by the business for other purposes.")
+    else:
+        royalty_income_per_month = [float(sys_param['royalty_income_per_month'][0])/1e3 if income == 0.0 else 0.0][0]
+        treasury_income_per_month = [float(sys_param['treasury_income_per_month'][0])/1e3 if income == 0.0 else 0.0][0]
+        other_income_per_month = [float(sys_param['other_income_per_month'][0])/1e3 if income == 0.0 else income][0]
+        one_time_payments_1 = [float(sys_param['one_time_payments_1'][0] + sys_param['one_time_payments_2'][0])/1e3 if expenditures == 0.0 else 0.0][0]
+        salaries_per_month = [float(sys_param['salaries_per_month'][0])/1e3 if expenditures == 0.0 else 0.0][0]
+        license_costs_per_month = [float(sys_param['license_costs_per_month'][0])/1e3 if expenditures == 0.0 else 0.0][0]
+        other_monthly_costs = [float(sys_param['other_monthly_costs'][0])/1e3 if expenditures == 0.0 else expenditures][0]
+    
+    st.write("**Buybacks and Burns**")
+    col91, col92, col93 = st.columns(3)
+    with col91:
+        enable_protocol_buybacks = st.toggle('Enable Protocol Token Buybacks', value=float(sys_param['buyback_perc_per_month'][0]) > 0 or float(sys_param['buyback_fixed_per_month'][0]) > 0, help=" Enable the buyback of tokens from a protocol bucket.")
+        enable_protocol_burning = st.toggle('Enable Protocol Token Burning', value=float(sys_param['burn_per_month'][0]) > 0, help=" Enable the burning of tokens from a protocol bucket.")
+    with col92:
+        if enable_protocol_buybacks:
+            buyback_type = st.radio('Buyback Type',('Fixed', 'Percentage'), index=0, help='The buyback type determines the buyback behavior of the business. A fixed buyback means that the business buys back a fixed USD worth amount of tokens per month. A percentage buyback means that the business buys back a percentage USD worth amount of tokens per month, depending on the business funds.')
+            if buyback_type == 'Fixed':
+                buyback_fixed_per_month = st.number_input('Buyback per month / $k', label_visibility="visible", min_value=0.0, value=[float(sys_param['buyback_fixed_per_month'][0])/1e3 if enable_protocol_buybacks else 0.0][0], disabled=False, key="buyback_fixed_per_month", help="The fixed USD worth amount of tokens bought back by the business per month.")
+                buyback_perc_per_month = 0.0
+            elif buyback_type == 'Percentage':
+                buyback_perc_per_month = st.number_input('Buyback per month / %', label_visibility="visible", min_value=0.0, value=[float(sys_param['buyback_perc_per_month'][0]) if enable_protocol_buybacks else 0.0][0], disabled=False, key="buyback_perc_per_month", help="The USD worth of tokens bought back by the business per month as percentage of the current business funds.")
+                buyback_fixed_per_month = 0.0
+            
+            buyback_buckets = ['Reserve', 'Community', 'Foundation', 'Incentivisation', 'Staking Vesting']
+            if not incentivisation_toggle:
+                buyback_buckets.pop(buyback_buckets.index('Incentivisation'))
+            if not staking_vesting_toggle:
+                buyback_buckets.pop(buyback_buckets.index('Staking Vesting'))
+            buyback_bucket = st.radio('Buyback Bucket',tuple(buyback_buckets), index=0, help='The buyback bucket determines the destination of the bought back tokens.')
+            buyback_start = st.date_input("Buybacks Start", min_value=datetime.strptime(sys_param['launch_date'][0], "%d.%m.%y"), value=datetime.strptime(sys_param['buyback_start'][0], "%d.%m.%y"), help="The date when monthly buybacks should start.")
+            buyback_end = st.date_input("Buybacks End", min_value=buyback_start, value=datetime.strptime(sys_param['buyback_end'][0], "%d.%m.%y"), help="The date when monthly buybacks should end.")
+
+        else:
+            buyback_perc_per_month = [float(sys_param['buyback_perc_per_month'][0]) if enable_protocol_buybacks else 0.0][0]
+            buyback_fixed_per_month = [float(sys_param['buyback_fixed_per_month'][0])/1e3 if enable_protocol_buybacks else 0.0][0]
+            buyback_bucket = [sys_param['buyback_bucket'][0] if enable_protocol_buybacks else 'Reserve'][0]
+            buyback_start = [datetime.strptime(sys_param['buyback_start'][0], "%d.%m.%y") if enable_protocol_buybacks else datetime.strptime(sys_param['launch_date'][0], "%d.%m.%y")][0]
+            buyback_end = [datetime.strptime(sys_param['buyback_end'][0], "%d.%m.%y") if enable_protocol_buybacks else datetime.strptime(sys_param['launch_date'][0], "%d.%m.%y")][0]
+    with col93:
+        if enable_protocol_burning:
+            burn_per_month = st.number_input('Burn per month / %', label_visibility="visible", min_value=0.0, value=[float(sys_param['burn_per_month'][0]) if enable_protocol_burning else 0.0][0], disabled=False, key="burn_per_month", help="The total supply percentage of tokens being burned from the determined protocol bucket per month.")
+            burn_start = st.date_input("Burning Start", min_value=datetime.strptime(sys_param['launch_date'][0], "%d.%m.%y"), value=datetime.strptime(sys_param['burn_start'][0], "%d.%m.%y"), help="The date when monthly burning should start.")
+            burn_end = st.date_input("Burning End", min_value=burn_start, value=datetime.strptime(sys_param['burn_end'][0], "%d.%m.%y"), help="The date when monthly burning should end.")
+            burn_buckets = ['Reserve', 'Community', 'Foundation']
+            burn_bucket = st.radio('Burn Bucket',tuple(burn_buckets), index=0, help='The burn bucket determines the protocol bucket origin of the burned tokens.')
+        else:
+            burn_per_month = [float(sys_param['burn_per_month'][0]) if enable_protocol_burning else 0.0][0]
+            burn_bucket = [sys_param['burn_bucket'][0] if enable_protocol_burning else 'Reserve'][0]
+            burn_start = [datetime.strptime(sys_param['burn_start'][0], "%d.%m.%y") if enable_protocol_burning else datetime.strptime(sys_param['launch_date'][0], "%d.%m.%y")][0]
+            burn_end = [datetime.strptime(sys_param['burn_end'][0], "%d.%m.%y") if enable_protocol_burning else datetime.strptime(sys_param['launch_date'][0], "%d.%m.%y")][0]
+    
     # Map new parameters to model input parameters
     new_params = {
         'equity_external_shareholders_perc': equity_perc,
@@ -669,6 +743,22 @@ def model_ui_inputs(input_file_path, uploaded_file, parameter_list):
         'avg_token_selling_allocation': [avg_token_selling_allocation if adoption_style == 'Custom' or show_full_adoption_table else adoption_dict[adoption_style]['avg_token_selling_allocation']][0],
         'avg_token_holding_allocation': [avg_token_holding_allocation if adoption_style == 'Custom' or show_full_adoption_table else adoption_dict[adoption_style]['avg_token_holding_allocation']][0],
         'avg_token_utility_removal': [avg_token_utility_removal if adoption_style == 'Custom' or show_full_adoption_table else adoption_dict[adoption_style]['avg_token_utility_removal']][0],
+        'royalty_income_per_month': royalty_income_per_month*1e3,
+        'treasury_income_per_month': treasury_income_per_month*1e3,
+        'other_income_per_month': other_income_per_month*1e3,
+        'one_time_payments_1': one_time_payments_1*1e3,
+        'salaries_per_month': salaries_per_month*1e3,
+        'license_costs_per_month': license_costs_per_month*1e3,
+        'other_monthly_costs': other_monthly_costs*1e3,
+        'buyback_perc_per_month': buyback_perc_per_month,
+        'buyback_fixed_per_month': buyback_fixed_per_month*1e3,
+        'buyback_bucket': buyback_bucket,
+        'buyback_start': buyback_start.strftime('%d.%m.%y'),
+        'buyback_end': buyback_end.strftime('%d.%m.%y'),
+        'burn_per_month': burn_per_month,
+        'burn_bucket': burn_bucket,
+        'burn_start': burn_start.strftime('%d.%m.%y'),
+        'burn_end': burn_end.strftime('%d.%m.%y'),
     }
 
     if lp_allocation < 0:
