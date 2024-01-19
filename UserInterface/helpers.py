34c2231501cffd639dc32986146d2c35f93f6af0
achimstruve <achim.struve@gmail.com>
2024-01-19 08:47:17 +0100

    parts: consolidate base ui components into function

--- UserInterface/helpers.py
+++ UserInterface/helpers.py
@@ -1,8 +1,15 @@
 from datetime import datetime
-from Model.parts.utils import *
 from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
 import pandas as pd
+import streamlit as st
+import sqlite3
+import numpy as np
+import os
+from PIL import Image
+
+from Model.parts.utils import *
+from UserInterface.plots import *
 
 def delete_parameter_and_simulation_data(param_id):
     # delete current selected parameter set and simulation data from database
@@ -66,3 +73,35 @@ def to_excel(df):
     writer.save()
     processed_data = output.getvalue()
     return processed_data
+
+def ui_base(parent_dir):
+    image = Image.open(parent_dir+'/images/ov_logo.jpg')
+    st.image(image, width=125)
+    st.title('Quantitative Token Model')
+
+    # get all existing project names
+    try:
+        db_sorted = get_simulation_data('simulationData.db', 'sys_param').sort_values('project_name', ascending=True)
+        project_names = db_sorted['project_name']
+        project_names = project_names.to_list()
+        project_names.append('')
+    except:
+        db_sorted = pd.DataFrame()
+        project_names = ['']
+    if 'project_name' not in st.session_state:
+        st.session_state['project_name'] = ''
+
+    if 'project_name' in st.session_state and 'param_id' in st.session_state and len(db_sorted) > 0:
+        st.session_state['project_name'] = st.sidebar.selectbox('Project Name', tuple(project_names), index=[db_sorted['id'].to_list().index(st.session_state['param_id']) if st.session_state['param_id'] in db_sorted['id'].to_list() else len(project_names)-1][0])
+        st.session_state['param_id'] = st.sidebar.text_input('Parameter ID',[db_sorted[db_sorted['project_name']==st.session_state['project_name']]['id'].iloc[0] if st.session_state['project_name'] in db_sorted['project_name'].to_list() else ""][0])
+    else:
+        st.session_state['project_name'] = st.sidebar.selectbox('Project Name', tuple(project_names), index=len(project_names)-1)
+        st.session_state['param_id'] = st.sidebar.text_input('Parameter ID', "")
+
+    try:
+        if st.session_state['param_id'] not in get_simulation_data('simulationData.db', 'sys_param')['id'].to_list():
+            st.sidebar.markdown(f"Parameter ID: {st.session_state['param_id']} does not exist. Please enter a valid parameter ID or run the simulation with your parameter set to get a parameter ID.")
+        else:
+            st.sidebar.markdown(f"This is a valid parameter ID ✅")
+    except:
+        pass
