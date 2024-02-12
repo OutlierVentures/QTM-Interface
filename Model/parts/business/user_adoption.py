e7349c8340142940568dbf8b0bbec2e4ba94a2ab
achimstruve <achim.struve@gmail.com>
2024-02-12 16:45:43 +0100

    fix: vary token holder adoption and UI advisor vesting duration from params

--- Model/parts/business/user_adoption.py
+++ Model/parts/business/user_adoption.py
@@ -93,7 +93,7 @@ def user_adoption_metrics(params, substep, state_history, prev_state, **kwargs):
         last_month_day = (pd.to_datetime(current_date) - pd.to_datetime('today')).days
         token_holders_last_month_regular = calculate_user_adoption(initial_token_holders,token_holders_after_10y,token_adoption_velocity,last_month_day)
         token_holders = (token_holders - token_holders_last_month_regular) + prev_token_holders
-        staking_apr_ratio = token_economy['te_staking_apr'] / agent_staking_apr_target
+        staking_apr_ratio = (token_economy['te_staking_apr'] / agent_staking_apr_target)**2
 
         token_holders = token_holders + (token_holders-prev_token_holders) * (staking_apr_ratio - 1) if staking_apr_ratio > 0 else token_holders
 
